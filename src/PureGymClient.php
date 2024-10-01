176f4c8c08c6378ee8c1e957d60dfb05e632631c
Owain Macbook <owain.jones74@gmail.com>
2024-10-01 01:12:55 +0100

    pull back access token

--- src/PureGymClient.php
+++ src/PureGymClient.php
@@ -5,13 +5,58 @@ namespace Owainjones74\Puregym;
 
 class PureGymClient
 {
-    private string $username;
-    private string $password;
+    public string $username;
+    public string $password;
+
+    public string $access_token;
 
     public function __construct(string $username, string $password)
     {
         $this->username = $username;
         $this->password = $password;
+
+        $this->access_token = $this->createToken();
     }
 
+    private function createToken(): string
+    {
+        $response = $this->curl('https://auth.puregym.com/connect/token', 'POST', [
+            'username' => $this->username,
+            'password' => $this->password,
+            'grant_type' => 'password',
+            'client_id' => 'ro.client',
+            'scope' => 'pgcapi'
+        ]);
+
+        if (!$response['access_token']) {
+            throw new \Exception('Could not get access token');
+        }
+
+        return $response['access_token'];
+    }
+
+    private function curl($url, $method = 'GET', $data = [])
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        }
+
+        $encodedData = '';
+        foreach ($data as $key => $value) {
+            $encodedData .= $key . '=' . $value . '&';
+        }
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/x-www-form-urlencoded',
+            'User-Agent: PureGym/1523 CFNetwork/1312 Darwin/21.0.0'
+        ]);
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($output, true);
+    }
 }
--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -3,3 +3,7 @@
 test('client initializes', function () {
     expect(client())->toBeInstanceOf(Owainjones74\Puregym\PureGymClient::class);
 });
+
+test('access token created', function () {
+    expect(client()->access_token)->toBeString();
+});
